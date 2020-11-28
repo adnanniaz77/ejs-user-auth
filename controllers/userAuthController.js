f35619f7c55e38c5f7d0ae4db866e55d1742f2f3
Adnan Niaz <adnanniaz77@yahoo.com>
2020-11-28 03:08:18 -0800

    created functionality for validation is done

--- controllers/userAuthController.js
+++ controllers/userAuthController.js
@@ -4,55 +4,78 @@ module.exports.home_get = (req, res) => {
     res.render('home');
 }
 
+// Function: Handle Errors for All routes
 const handleError = (err) => {
-    return err.message
+    let errors = { username: "", email: "", password: ""}
+
+    // check for duplicate email
+    if (err.code === 11000) {
+        errors.email = "Email already exists, please try another one"
+        return errors;
+    }
+    
+    // validation errors
+    Object.values(err.errors).forEach(({properties}) => {
+        errors[properties.path] = properties.message;
+    })
+
+    return errors;
 }
 
-/* ------------------------------ 
+/* 
+------------------------------ 
         GET "register" page
- ------------------------------ */
+------------------------------ 
+*/
 
 module.exports.register_get = (req, res) => {
     res.render('register')
 }
 
-/* ------------------------------
+/* 
+------------------------------
         GET "login" page
- ------------------------------ */
+------------------------------ 
+ */
 
 module.exports.login_get = (req, res) => {
     res.render('login')
 }
 
-/* ------------------------------ 
+/* 
+------------------------------ 
         GET "dashboard"
- ------------------------------ */
+------------------------------ 
+*/
 
 module.exports.dashboard_get = (req, res) => {
     res.render('dashboard')
 }
 
-/* ------------------------------
-        POST "register" new user
- ------------------------------ */
+/* 
+------------------------------
+        POST "register" new
+------------------------------ 
+*/
 
 module.exports.register_post = async(req, res) => {
     const { username,  email, password } = req.body;
     
     try {
         const newUser = await User.create({username, email, password});
-        if(!newUser) res.status(400).json({msg: 'Something went wrong'})
-        res.status(201).json({User: newUser})
+        res.status(201).json(newUser)
     } 
     catch (err) {
         const error = handleError(err)
-        res.status(500).json({Error: error})
+        res.status(400).send({Error: "user not created", error})
     }
 }
 
-/* ------------------------------ 
+/* 
+------------------------------ 
           POST "login" user
- ------------------------------ */
+------------------------------
+*/
 
 module.exports.login_post = (req, res) => {
     const { email, password } = req.body;
--- model/User.js
+++ model/User.js
@@ -4,20 +4,20 @@ const {isEmail} = require('validator')
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        minlength: 6,
-        required: true
+        minlength: [6, 'Minimum username length is 6 characters'],
+        required: [true, 'Please enter a username']
     },
     email: {
         type: String,
         unique: true,
         lowercase: true,
-        required: true,
+        required: [true, 'Please enter an email'],
         validate: [isEmail, 'Please enter a valid email']
     },
     password: {
         type: String,
-        minlength: 6,
-        required: true
+        minlength: [6, 'Minimum password length is 6 characters'],
+        required: [true, 'Please enter a password']
     }
 })
 
